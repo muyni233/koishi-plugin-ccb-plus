700494dd533a1a48bc6b97f7eee46481395cf784
muyni <105572218+muyni233@users.noreply.github.com>
2025-10-28 20:02:28 +0800

    Add files via upload

---
+++ lib/index.d.ts
@@ -0,0 +1,46 @@
+import { Context, Schema } from 'koishi';
+export declare const name = "ccb-plus";
+export interface CCBConfig {
+    ywWindow: number;
+    ywThreshold: number;
+    ywBanDuration: number;
+    ywProbability: number;
+    whiteList: string[];
+    selfCcb: boolean;
+    critProb: number;
+    isLog: boolean;
+}
+export interface CCBData {
+    [groupId: string]: CCBGroupData[];
+}
+export interface CCBActorInfo {
+    count: number;
+    first: boolean;
+    max: boolean;
+}
+export interface CCBGroupData {
+    id: string;
+    num: number;
+    vol: number;
+    ccb_by: {
+        [actorId: string]: CCBActorInfo;
+    };
+    max: number;
+}
+export interface CCBLogEntry {
+    group: string;
+    executor: string;
+    target: string;
+    time: number;
+    vol: string;
+}
+export interface CCBActionTime {
+    [userId: string]: number[];
+}
+declare module 'koishi' {
+    interface Tables {
+        ccb_data: CCBData;
+    }
+}
+export declare const Config: Schema<CCBConfig>;
+export declare function apply(ctx: Context, config: CCBConfig): void;
---
+++ readme.md
@@ -0,0 +1,11 @@
+# koishi-plugin-ccb-plus
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-ccb-plus?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-ccb-plus)
+
+Koshi插件，与群友发生ccb行为。(移植自astrbot_plugin_ccb_plus)
+
+原插件：[Koikokokokoro/astrbot_plugin_ccb_plus](https://github.com/Koikokokokoro/astrbot_plugin_ccb_plus)
+
+以后会进行小幅修改
+
+> 使用Qwen3-Coder协助完成
